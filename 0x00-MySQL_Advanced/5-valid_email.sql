00d8707c68a512c398d477738caa815c5976dc31
eddie9110 <edwimaregwa@gmail.com>
2024-03-22 09:26:18 +0000

    Capitalised SQL keyword ON

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,10 @@
 -- script that creates a trigger that resets the attribute valid_email only when the email has been changed.
 
-DELIMITER //
 CREATE TRIGGER reset_email
-AFTER UPDATE on users
+AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0
     END IF;
-END //
-DELIMITER ;
+END;
